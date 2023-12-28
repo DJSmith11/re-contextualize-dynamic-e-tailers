63027d470726843205f755a671f281ae82229c77
DJSmith11 <142458119+DJSmith11@users.noreply.github.com>
2023-12-28 16:18:15 +0100

    Create code_2.py

---
+++ code_2.py
@@ -0,0 +1,40 @@
+    return data
+import random
+
+    data = generate_random_data()
+    for item in data:
+    data = [random.randint(1, 100) for _ in range(10)]
+        print(f"Random Number: {item}")
+def generate_random_data():
+def main():
+if __name__ == "__main__":
+
+    main()
+
+if __name__ == "__main__":
+import random
+    data = [random.randint(1, 100) for _ in range(10)]
+def generate_random_data():
+
+
+    for item in data:
+    main()
+    data = generate_random_data()
+
+def main():
+        print(f"Random Number: {item}")
+    return data
+
+if __name__ == "__main__":
+def main():
+
+    data = generate_random_data()
+    return data
+        print(f"Random Number: {item}")
+    for item in data:
+    main()
+import random
+
+    data = [random.randint(1, 100) for _ in range(10)]
+
+def generate_random_data():
