a322788fc6da037b400f1f91a55632d022244cea
DJSmith11 <142458119+DJSmith11@users.noreply.github.com>
2023-12-28 19:20:30 +0100

    Create code_1.py

---
+++ code_1.py
@@ -0,0 +1,41 @@
+        for (int i = 0; i < 10; i++) {
+
+        for (int item : data) {
+public class RandomDataGenerator {
+        }
+            data[i] = (int) (Math.random() * 100) + 1;
+
+}
+        }
+        int[] data = new int[10];
+    }
+            System.out.println("Random Number: " + item);
+    public static void main(String[] args) {}
+    const data = [];
+        console.log("Random Number: " + item);
+main();
+function main() {
+    });
+    }
+    return data;
+}
+
+
+function generateRandomData() {
+    data.forEach(item => {
+
+    for (let i = 0; i < 10; i++) {
+    const data = generateRandomData();
+        data.push(Math.floor(Math.random() * 100) + 1);            data[i] = (int) (Math.random() * 100) + 1;
+        for (int i = 0; i < 10; i++) {
+public class RandomDataGenerator {
+}
+    public static void main(String[] args) {
+
+        }
+    }
+        }
+        int[] data = new int[10];
+
+        for (int item : data) {
+            System.out.println("Random Number: " + item);
